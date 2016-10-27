32f660bb41f4ee8aa909214db3436b9a0df3884e
skybber <lada.dvorak7@gmail.com>
2016-10-27 20:04:32 +0200

    Deltaspike 1.7 compatibility problems fixed

--- plugin/hotswap-agent-deltaspike-plugin/src/main/java/org/hotswap/agent/plugin/deltaspike/proxy/PartialBeanClassRefreshAgent.java
+++ plugin/hotswap-agent-deltaspike-plugin/src/main/java/org/hotswap/agent/plugin/deltaspike/proxy/PartialBeanClassRefreshAgent.java
@@ -1,6 +1,9 @@
 package org.hotswap.agent.plugin.deltaspike.proxy;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+import javax.enterprise.inject.spi.BeanManager;
 
 import org.apache.deltaspike.core.api.provider.BeanManagerProvider;
 import org.apache.deltaspike.partialbean.impl.PartialBeanProxyFactory;
@@ -19,10 +22,18 @@ public class PartialBeanClassRefreshAgent {
             Thread.currentThread().setContextClassLoader(classLoader);
             Object lifecycle = (Object) ReflectionHelper.get(partialBean, "lifecycle");
             if (lifecycle != null) {
-                Class<? extends InvocationHandler> delegateInvocationHandlerClass = (Class) ReflectionHelper.get(lifecycle, "delegateInvocationHandlerClass");
                 Class<?> targetClass = (Class) ReflectionHelper.get(lifecycle, "targetClass");
                 PartialBeanProxyFactory proxyFactory = PartialBeanProxyFactory.getInstance();
-                proxyFactory.getProxyClass(BeanManagerProvider.getInstance().getBeanManager(), targetClass, delegateInvocationHandlerClass);
+                try {
+                    // Deltaspike 1.5
+                    Method m3 = PartialBeanProxyFactory.class.getMethod("getProxyClass", new Class[] { BeanManager.class, Class.class, Class.class} );
+                    Class<? extends InvocationHandler> delegateInvocationHandlerClass = (Class) ReflectionHelper.get(lifecycle, "delegateInvocationHandlerClass");
+                    m3.invoke(proxyFactory, new Object[] {BeanManagerProvider.getInstance().getBeanManager(), targetClass, delegateInvocationHandlerClass} );
+                } catch (NoSuchMethodException e) {
+                    // Deltaspike 1.7
+                    Method m2 = PartialBeanProxyFactory.class.getMethod("getProxyClass", new Class[] { BeanManager.class, Class.class } );
+                    m2.invoke(proxyFactory, new Object[] {BeanManagerProvider.getInstance().getBeanManager(), targetClass} );
+                }
             }
         } catch (Exception e) {
             LOGGER.error("Deltaspike proxy redefinition failed", e);
