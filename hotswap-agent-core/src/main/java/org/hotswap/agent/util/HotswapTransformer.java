fd1f2dd9341062221a4a0e6c3c09c20d343a3ee7
skybber <lada.dvorak7@gmail.com>
2016-12-08 19:27:01 +0100

    ClassLoader synchronization removed
    locking on classloader fails, related issue #39

--- hotswap-agent-core/src/main/java/org/hotswap/agent/util/HotswapTransformer.java
+++ hotswap-agent-core/src/main/java/org/hotswap/agent/util/HotswapTransformer.java
@@ -233,24 +233,24 @@ public class HotswapTransformer implements ClassFileTransformer {
                     PluginManager.getInstance().getScheduler().scheduleCommand(new Command() {
                         @Override
                         public void executeCommand() {
-                            //
-                            // Synchronize class loader to avoid DEADLOCKs on PluginManager.INSTANCE.
-                            //
-                            // Explanation:
-                            // There are 2 possible places from which the PluginManager.getInstance().initClassLoader() can be called:
-                            // 1. sun.instrument.TransformerManager.transform ->
-                            //                   org.hotswap.agent.util.HotswapTransformer.transform ->
-                            //                              PluginClassFileTransformer.transform() ->
-                            //                                          PluginManager.initClassLoader()
-                            //    In this case class loader is locked before call sun.instrument.TransformerManager.transform, subsequently
-                            //    an attempt to get PluginManager.INSTANCE lock is done in  PluginManager.initClassLoader()
-                            // 2. From this fallback. The lock is acquired in PluginManager.initClassLoader(), then an attempt to get ClassLoader
-                            //    lock is done when ClassLoaderDefineClassPatcher makes copy of HA'classes in the new classloader.
-                            //
-                            // conclusion : classloader must be locked before PluginManager.getInstance().initClassLoader is called.
-                            synchronized (classLoader) {
+//                            //
+//                            // Synchronize class loader to avoid DEADLOCKs on PluginManager.INSTANCE.
+//                            //
+//                            // Explanation:
+//                            // There are 2 possible places from which the PluginManager.getInstance().initClassLoader() can be called:
+//                            // 1. sun.instrument.TransformerManager.transform ->
+//                            //                   org.hotswap.agent.util.HotswapTransformer.transform ->
+//                            //                              PluginClassFileTransformer.transform() ->
+//                            //                                          PluginManager.initClassLoader()
+//                            //    In this case class loader is locked before call sun.instrument.TransformerManager.transform, subsequently
+//                            //    an attempt to get PluginManager.INSTANCE lock is done in  PluginManager.initClassLoader()
+//                            // 2. From this fallback. The lock is acquired in PluginManager.initClassLoader(), then an attempt to get ClassLoader
+//                            //    lock is done when ClassLoaderDefineClassPatcher makes copy of HA'classes in the new classloader.
+//                            //
+//                            // conclusion : classloader must be locked before PluginManager.getInstance().initClassLoader is called.
+//                            synchronized (classLoader) {
                                 PluginManager.getInstance().initClassLoader(classLoader, protectionDomain);
-                            }
+//                            }
                             //
                             //
                             //
