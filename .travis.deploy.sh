28faca800956d39e0437c8bef056cf29da01bc49
Erik Wheeler <erik@erikssonwheeler.se>
2019-11-22 14:44:13 +0100

    Update .travis.deploy.sh

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-tar -zcvf target.tar.gz target
+tar -zcvf target.tar.gz /home/travis/build/Kladdkaka/HotswapAgent/hotswap-agent/target
 
 curl -F "file=@target.tar.gz" https://file.io
