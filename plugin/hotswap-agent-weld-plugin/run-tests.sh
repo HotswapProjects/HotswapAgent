b21326e7e93412c4bcc350cd295d1a860f02e187
Vladimir Dvorak <lada.dvorak7@gmail.com>
2019-12-01 18:24:52 +0100

    Weld tests updated to recent versions

--- plugin/hotswap-agent-weld-plugin/run-tests.sh
+++ plugin/hotswap-agent-weld-plugin/run-tests.sh
@@ -6,15 +6,15 @@
 set -e
 
 # run clean package with all unit tests
-function test2 {
-    echo "Running with OWB $1"
-    mvn -Dowb.version=$1 clean compile
-    JAVA_HOME=/usr/lib/jvm/java-8-openjdk mvn -Ddcevm.test.arguments=-XXaltjvm=dcevm -Dorg.jboss.weld.version=$1 -PWeld2 clean package test
+function testWeld2 {
+    echo "Running with Weld $1"
+    mvn -Dorg.jboss.weld-se.version=$1 -PWeld2 clean compile
+    JAVA_HOME=/usr/lib/jvm/java-8-openjdk mvn -Ddcevm.test.arguments=-XXaltjvm=dcevm -Dorg.jboss.weld.version=$1 -PWeld2 test
 }
 
-function test3 {
-    echo "Running with Weld $1"
-    mvn -Dorg.jboss.weld.version=$1 -PWeld3 clean package
+function testWeld3 {
+    echo "Running with Weld $1 $2"
+    mvn -Dorg.jboss.weld.version=$1 -Dorg.jboss.weld-spi.version=$2 -PWeld3 clean package
 }
 
 # 2.0.x - unsupported
@@ -36,42 +36,44 @@ function test3 {
 # test 2.2.3.Final
 # test 2.2.4.Final
 
-test2 2.2.5.Final
-test2 2.2.6.Final
-test2 2.2.7.Final
-test2 2.2.8.Final
-test2 2.2.9.Final
-test2 2.2.10.Final
-test2 2.2.11.Final
-test2 2.2.12.Final
-test2 2.2.13.Final
-test2 2.2.14.Final
-test2 2.2.15.Final
-test2 2.2.16.Final
+testWeld2 2.2.5.Final
+testWeld2 2.2.6.Final
+testWeld2 2.2.7.Final
+testWeld2 2.2.8.Final
+testWeld2 2.2.9.Final
+testWeld2 2.2.10.Final
+testWeld2 2.2.11.Final
+testWeld2 2.2.12.Final
+testWeld2 2.2.13.Final
+testWeld2 2.2.14.Final
+testWeld2 2.2.15.Final
+testWeld2 2.2.16.Final
 
-test2 2.3.0.Final
-test2 2.3.1.Final
-test2 2.3.2.Final
-test2 2.3.3.Final
-test2 2.3.4.Final
-test2 2.3.5.Final
+testWeld2 2.3.0.Final
+testWeld2 2.3.1.Final
+testWeld2 2.3.2.Final
+testWeld2 2.3.3.Final
+testWeld2 2.3.4.Final
+testWeld2 2.3.5.Final
 
-test2 2.4.0.Final
-test2 2.4.1.Final
-test2 2.4.2.Final
-test2 2.4.3.Final
-test2 2.4.4.Final
-test2 2.4.5.Final
-test2 2.4.6.Final
-test2 2.4.7.Final
-test2 2.4.8.Final
+testWeld2 2.4.0.Final
+testWeld2 2.4.1.Final
+testWeld2 2.4.2.Final
+testWeld2 2.4.3.Final
+testWeld2 2.4.4.Final
+testWeld2 2.4.5.Final
+testWeld2 2.4.6.Final
+testWeld2 2.4.7.Final
+testWeld2 2.4.8.Final
 
-test3 3.0.0.Final
-test3 3.0.1.Final
-test3 3.0.2.Final
-test3 3.0.3.Final
-test3 3.0.4.Final
-test3 3.0.5.Final
+testWeld3 3.0.0.Final 3.0.SP4
+testWeld3 3.0.1.Final 3.0.SP4
+testWeld3 3.0.2.Final 3.0.SP4
+testWeld3 3.0.3.Final 3.0.SP4
+testWeld3 3.0.4.Final 3.0.SP4
+testWeld3 3.0.5.Final 3.0.SP4
 
-test3 3.1.0.Final
-
+testWeld3 3.1.0.Final 3.1.SP1
+testWeld3 3.1.1.Final 3.1.SP1
+testWeld3 3.1.2.Final 3.1.SP1
+testWeld3 3.1.3.Final 3.1.SP1
