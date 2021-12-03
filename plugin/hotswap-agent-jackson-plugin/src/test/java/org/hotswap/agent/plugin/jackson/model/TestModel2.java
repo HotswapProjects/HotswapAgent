bff25b9f43b0cbc6f2931bb66b26f260e09b2020
liuzhengyang <liuzhengyang75@gmail.com>
2021-12-03 16:22:56 +0800

    jackson plugin

---
+++ plugin/hotswap-agent-jackson-plugin/src/test/java/org/hotswap/agent/plugin/jackson/model/TestModel2.java
@@ -0,0 +1,26 @@
+package org.hotswap.agent.plugin.jackson.model;
+
+/**
+ * @author liuzhengyang
+ * 2021/12/3
+ */
+public class TestModel2 {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
