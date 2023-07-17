d1fbb4b59b3639bc0e40a7d4c1e4830d514b25ec
zhouzh_zzz <zhouzh_zzz@qq.com>
2023-07-17 14:36:55 +0800

    hotswap @transactional

--- plugin/hotswap-agent-spring-plugin/src/test/java/org/hotswap/agent/plugin/spring/transactional/TransactionalApplication.java
+++ plugin/hotswap-agent-spring-plugin/src/test/java/org/hotswap/agent/plugin/spring/transactional/TransactionalApplication.java
@@ -4,10 +4,6 @@ package org.hotswap.agent.plugin.spring.transactional;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-/**
- * @author: 周子恒
- * @create: 2023-05-31 13:49
- **/
 @ComponentScan
 @EnableTransactionManagement
 public class TransactionalApplication {
