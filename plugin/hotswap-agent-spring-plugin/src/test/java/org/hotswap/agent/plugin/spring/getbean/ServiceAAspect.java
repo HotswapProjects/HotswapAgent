99ef94f7f1d3dca08f412e243d96a40ea5230420
zzhlhc <zhouzh_zzz@qq.com>
2023-10-06 17:33:00 +0800

    Fix getBean OOM

---
+++ plugin/hotswap-agent-spring-plugin/src/test/java/org/hotswap/agent/plugin/spring/getbean/ServiceAAspect.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2013-2023 the HotswapAgent authors.
+ *
+ * This file is part of HotswapAgent.
+ *
+ * HotswapAgent is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * HotswapAgent is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with HotswapAgent. If not, see http://www.gnu.org/licenses/.
+ */
+package org.hotswap.agent.plugin.spring.getbean;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ServiceAAspect {
+    @Around("execution(* org.hotswap.agent.plugin.spring.getbean.ServiceA.service(..))")
+    public Object around(ProceedingJoinPoint pjp) throws Throwable {
+        return pjp.proceed() + " and aspect";
+    }
+}
