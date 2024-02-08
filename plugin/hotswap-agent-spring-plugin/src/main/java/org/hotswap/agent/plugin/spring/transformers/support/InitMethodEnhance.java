31f2f33179528a3516dc1c45a088c26fc6bcff2b
cvictory <shenglicao2@gmail.com>
2024-02-08 10:47:58 +0800

    fix review issue: make the enhance code intent

--- plugin/hotswap-agent-spring-plugin/src/main/java/org/hotswap/agent/plugin/spring/transformers/support/InitMethodEnhance.java
+++ plugin/hotswap-agent-spring-plugin/src/main/java/org/hotswap/agent/plugin/spring/transformers/support/InitMethodEnhance.java
@@ -6,16 +6,16 @@ public class InitMethodEnhance {
         boolean hasReturnValue) {
         return "{"
             + "if (org.hotswap.agent.plugin.spring.reload.BeanFactoryAssistant.getBeanFactoryAssistant($0) == null || "
-            + "!org.hotswap.agent.plugin.spring.reload.BeanFactoryAssistant.getBeanFactoryAssistant($0).isReload"
-            + "()) {"
-            + " throw " + exceptionName + " ; }"
+            + "!org.hotswap.agent.plugin.spring.reload.BeanFactoryAssistant.getBeanFactoryAssistant($0).isReload()) {"
+                + " throw " + exceptionName + " ; }"
             + "else {"
-            + loggerName + ".warning(\"Failed to invoke init method of {} from {}: {}\", new java.lang.Object[]{"
-            + objectName + ".getClass().getName(),\""
-            + from + "\", " + exceptionName + ".getMessage()"
-            + "});"
-            + loggerName + ".debug(\"the detail reason as following{}: \", " + exceptionName + ", new java.lang.Object[]{ "
-            + "\" \"});"
-            + (hasReturnValue ? " return " + objectName + ";" : "return;") + " } }";
+                + loggerName + ".warning(\"Failed to invoke init method of {} from {}: {}\", "
+                    + "new java.lang.Object[]{" + objectName + ".getClass().getName(),\"" + from + "\", "
+                    + exceptionName + ".getMessage()});"
+                + loggerName + ".debug(\"the detail reason as following{}: \", " + exceptionName
+                    + ", new java.lang.Object[]{ \" \"});"
+                + (hasReturnValue ? " return " + objectName + ";" : "return;")
+            + " } "
+            + "}";
     }
 }
