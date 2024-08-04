34b96e90b4b04fb3b82df7b32d8e9ff31a372b90
Vladimir Dvorak <lada.dvorak7@gmail.com>
2024-08-04 13:41:03 +0200

    update tested version in OwbJakarta plugin

--- plugin/hotswap-agent-owb-jakarta-plugin/src/main/java/org/hotswap/agent/plugin/owb_jakarta/OwbJakartaPlugin.java
+++ plugin/hotswap-agent-owb-jakarta-plugin/src/main/java/org/hotswap/agent/plugin/owb_jakarta/OwbJakartaPlugin.java
@@ -61,8 +61,8 @@ import org.hotswap.agent.watch.Watcher;
  */
 @Plugin(name = "OwbJakarta",
         description = "OpenWebBeans Jakarta framework(http://openwebbeans.apache.org/). Reload, reinject bean, redefine proxy class after bean class definition/redefinition.",
-        testedVersions = {"2.0.27"},
-        expectedVersions = {"All between 2.0.27"},
+        testedVersions = {"4.0.2"},
+        expectedVersions = {"4.x"},
         supportClass = { BeansDeployerTransformer.class, CdiContextsTransformer.class, ProxyFactoryTransformer.class, AbstractProducerTransformer.class })
 public class OwbJakartaPlugin {
 
