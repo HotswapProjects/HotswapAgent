228503d34282c39890ee97dfc6691f3ad24f4a76
homejim <454690042@qq.com>
2024-10-26 15:52:59 +0800

    Fix Mybatis plugin: Remove potential code errors. (#607)
    * Fix the issue in MyBatis-Spring mode where a NullPointerException (NPE) occurs during reload, and also differentiate between MyBatis and MyBatis-Spring modes
    
    * Update MyBatisTransformers.java
    
    Fix the issue where using the insertAt method might fail due to changes in the processPropertyPlaceHolders method body. Add a list variable at the beginning of the method, include the factory in the list after factory initialization, and finally execute the removal logic at the end of the method.
    
    Why not execute the removal right after initialization? I tried that, but it fails during reload.
    
    * Remove the logging code; in lower versions of spring-mybatis, there is no getMapperInterface method, which causes errors.
    
    ---------
    
    Co-authored-by: hejiajin <Homejim5>

--- plugin/hotswap-agent-mybatis-plugin/src/main/java/org/hotswap/agent/plugin/mybatis/proxy/SpringMapperFactoryBean.java
+++ plugin/hotswap-agent-mybatis-plugin/src/main/java/org/hotswap/agent/plugin/mybatis/proxy/SpringMapperFactoryBean.java
@@ -15,24 +15,20 @@ public class SpringMapperFactoryBean {
     protected static final List<MapperFactoryBean<Object>> mappingFactoryBeans = new LinkedList<>();
 
     public static void registerMapperFactoryBean(MapperFactoryBean<Object> factoryBean) {
-        Class mapperInterface = (Class) ReflectionHelper.invoke(factoryBean, "getMapperInterface");
-
-        LOGGER.debug("ready to add factory bean:" + mapperInterface.getName());
         for (MapperFactoryBean<Object> addedMappingFactoryBean : mappingFactoryBeans) {
             if (Objects.equals(factoryBean, addedMappingFactoryBean)) {
                 return;
             }
         }
         mappingFactoryBeans.add(factoryBean);
-        LOGGER.info("add factoryBean:" + mapperInterface.getName());
+        LOGGER.debug("add factoryBean:" + factoryBean);
     }
 
     public static void reload() {
         for (MapperFactoryBean<Object> mappingFactoryBean : mappingFactoryBeans) {
             try {
-                Class mapperInterface = (Class) ReflectionHelper.invoke(mappingFactoryBean, "getMapperInterface");
                 ReflectionHelper.invoke(mappingFactoryBean, "checkDaoConfig");
-                LOGGER.info("reload factoryBean: " + mapperInterface.getName());
+                LOGGER.reload("reload factoryBean: " + mappingFactoryBean);
             } catch (Exception e) {
                 LOGGER.error("reload factoryBean error: " + e.getMessage());
             }
