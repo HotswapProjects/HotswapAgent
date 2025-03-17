f6bc0e4190e3a0e6cba79192e840a1b1067d5b7e
Artur Signell <artur@vaadin.com>
2025-03-17 12:11:38 +0200

    Support adding entities and repositories

---
+++ plugin/hotswap-agent-spring-plugin/src/main/java/org/hotswap/agent/plugin/spring/reload/ReloadRepositories.java
@@ -0,0 +1,33 @@
+package org.hotswap.agent.plugin.spring.reload;
+
+import org.hotswap.agent.logging.AgentLogger;
+import org.hotswap.agent.util.ReflectionHelper;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+
+public class ReloadRepositories {
+
+    private static Object repositoryConfigurationDelegate;
+    private static Object registry;
+    private static Object repositoryConfigurationExtension;
+
+    private static final AgentLogger LOGGER = AgentLogger.getLogger(ReloadRepositories.class);
+
+    public static void storeReferences(Object repositoryConfigurationDelegate, Object registry,
+            Object repositoryConfigurationExtension) {
+        ReloadRepositories.repositoryConfigurationDelegate = repositoryConfigurationDelegate;
+        ReloadRepositories.registry = registry;
+        ReloadRepositories.repositoryConfigurationExtension = repositoryConfigurationExtension;
+
+    }
+
+    public static void reloadAllRepositories() {
+        LOGGER.trace("Rescanning and reloading all JPA repositories");
+        ReflectionHelper.invoke(repositoryConfigurationDelegate, repositoryConfigurationDelegate.getClass(),
+                "registerRepositoriesIn",
+                new Class[] { BeanDefinitionRegistry.class,
+                        ScanNewEntities.findClass(
+                                "org.springframework.data.repository.config.RepositoryConfigurationExtension") },
+                registry,
+                repositoryConfigurationExtension);
+    }
+}
---
+++ plugin/hotswap-agent-spring-plugin/src/main/java/org/hotswap/agent/plugin/spring/reload/ScanNewEntities.java
@@ -0,0 +1,38 @@
+package org.hotswap.agent.plugin.spring.reload;
+
+import org.hotswap.agent.logging.AgentLogger;
+import org.hotswap.agent.util.ReflectionHelper;
+
+public class ScanNewEntities {
+
+    private static final AgentLogger LOGGER = AgentLogger.getLogger(ScanNewEntities.class);
+    private static Object persistenceManagedTypesScanner;
+    private static Object packagesToScan;
+
+    public static void storeScanner(Object persistenceManagedTypesScanner, Object packagesToScan) {
+        ScanNewEntities.persistenceManagedTypesScanner = persistenceManagedTypesScanner;
+        ScanNewEntities.packagesToScan = packagesToScan;
+    }
+
+    public static void scanNewEntities(Object localContainerEntityManagerFactoryBean) {
+        LOGGER.trace("Rescanning for entities");
+        Object persistenceManagedTypes = ReflectionHelper.invoke(persistenceManagedTypesScanner,
+                persistenceManagedTypesScanner.getClass(), "scan",
+                new Class[] { String[].class }, packagesToScan);
+        ReflectionHelper.invoke(localContainerEntityManagerFactoryBean,
+                localContainerEntityManagerFactoryBean.getClass(), "setManagedTypes",
+                new Class[] { findClass("org.springframework.orm.jpa.persistenceunit.PersistenceManagedTypes") },
+                persistenceManagedTypes);
+
+        ReflectionHelper.invoke(localContainerEntityManagerFactoryBean, "afterPropertiesSet");
+    }
+
+    static Class<?> findClass(String string) {
+        try {
+            return Class.forName(string);
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("Class not found: " + string, e);
+        }
+    }
+
+}
